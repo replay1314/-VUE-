16ba357f4a81de9aa7b20a224c271ecd28f4bdf3
replay1314 <118278624+replay1314@users.noreply.github.com>
2023-04-21 14:38:54 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    lintOnSave: false,
+    devServer: {
+        port: 9001,
+        proxy: {
+            '/': {
+                target: 'http://39.106.127.16:7001/',
+                ws:false
+            }
+        },
+    }
+
 })
